5c656e51b324f9d8ec371480047be7e102dac7a7
John Crespi Oncher <johnoncher@gmail.com>
2019-01-05 05:10:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,7 +12,6 @@ songs = [
 ]
 
 def help
-  binding.pry
   puts "I accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
@@ -23,8 +22,38 @@ end
 def list(songs)
 
   songs.each_with_index do | song , i|
-    puts ". " + song 
-  
+    puts
+    puts "#{i+1}. #{song}"
+  end
 end
-  
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_choice = gets.chomp
+  range = 1..songs.length
+  if range.include?(song_choice.to_i)
+    puts "Playing #{songs[song_choice.to_i-1]}"
+  elsif songs.include?(song_choice)
+    puts "Playing #{song_choice}"
+  else puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  command = gets.chomp
+  if "list" == command
+    list(songs)
+  elsif "play" == command
+    play(songs)
+  elsif "exit" == command
+    exit_jukebox
+    
+  elsif "help" == command
+    help
+  end
 end
